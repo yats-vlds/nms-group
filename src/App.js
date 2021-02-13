047c6c8993e0476516dece4d8f1484d7f66a7f14
Lexun <59420596+lexun1060@users.noreply.github.com>
2021-02-13 10:51:35 +0200

    Why me part completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Cap from "./components/Cap";
 import StarProject from "./components/StarProject";
 import Expert from "./components/Expert";
-import WhyMe from "./components/WhyMe";
+import WhyMe from "./components/Why";
 import Jobs from "./components/Jobs";
 import Footer from "./components/Footer";
 
