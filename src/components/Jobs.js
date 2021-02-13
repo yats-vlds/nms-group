29a4f4328ea8a3451174f6f8b2cc50e2eeb03196
Lexun <59420596+lexun1060@users.noreply.github.com>
2021-02-13 15:12:51 +0200

    add Jobs work

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -5,6 +5,13 @@ import shadow from "../assets/shadow.png"
 import sloygoPrint from "../assets/sloygoPrint.svg"
 import goPrintOriginal from "../assets/goPrintOriginal.svg"
 import inmore  from "../assets/inmore.svg"
+import shadow2 from "../assets/shadowTwo.png"
+import tableTwoTwo from "../assets/tableTwoTwo.svg"
+import table1pryamogolnik from "../assets/table1Pryamogolnik.svg"
+import tableThree1 from "../assets/tableThree1.png"
+import tableTree2 from "../assets/tableThree2.svg"
+import tableThree3 from "../assets/tableThree3.svg"
+import tableThree4 from "../assets/tableThree4.svg"
 
 const Jobs = () => {
     return (
@@ -19,7 +26,7 @@ const Jobs = () => {
                     <div className="bootstrap__tableLineOne"></div>
                     <div className="bootstrap__tableLineOne"></div>
                     <img src={shadow} className="icon2"/>
-                    <img src={pryamugolnik} className="icon"/>
+                    <img src={table1pryamogolnik} className="icon"/>
                     <img src={sloygoPrint} className="icon3"/>
                     <img src={goPrintOriginal} className="icon4"/>
                 </div>
@@ -84,20 +91,34 @@ const Jobs = () => {
                     <div className="bootstrap__tableLineTwo"></div>
                     <div className="bootstrap__tableLineTwo"></div>
                     <div className="bootstrap__tableLineTwo"></div>
-                    <img src={shadow} className="icon2TableTwo"/>
-                    <img src={pryamugolnik} className="iconTableTwo"/>
+                    <img src={shadow2} className="iconTableTwo"/>
+                    <img src={tableTwoTwo} className="icon2TableTwo"/>
                     <img src={inmore} className="icon3TableTwo"/>
                 </div>
             </div>
 
-            <div className="bootstrap__tableTwo">
-                <div className="bootstrap__tableTwoLeft">
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableTwoLeftInfo">
+
+            <div className="bootstrap__table">
+                <div className="bootstrap__tableLeft">
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableLineThree"></div>
+                    <img src={tableThree1} className="iconTableThree"/>
+                    <img src={tableTree2} className="icon2TableThree"/>
+                    <img src={tableThree3} className="icon3TableThree"/>
+                    <img src={tableThree4} className="icon4TableThree"/>
+                </div>
+                <div className="bootstrap__tableRight">
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableLineThree"></div>
+                    <div className="bootstrap__tableRightInfo">
                         <h1 className="bootstrap__tableTitle" style={{marginTop: "180px", marginBottom: "23px"}}>
                             Project name
                         </h1>
@@ -116,19 +137,8 @@ const Jobs = () => {
                         </h1>
                     </div>
                 </div>
-                <div className="bootstrap__tableLeft">
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <div className="bootstrap__tableLineTwo"></div>
-                    <img src={shadow} className="icon2TableTwo"/>
-                    <img src={pryamugolnik} className="iconTableTwo"/>
-                    <img src={inmore} className="icon3TableTwo"/>
-                </div>
             </div>
+
         </>
     )
 }
