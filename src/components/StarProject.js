06f5b11fff3d38a3ec0ccd1a1ac506fb2f657790
Lexun <59420596+lexun1060@users.noreply.github.com>
2021-02-13 09:12:23 +0200

    completed star

--- src/components/StarProject.js
+++ src/components/StarProject.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./StarProject.css"
 import background from "../assets/back.svg"
+import iconDown from "../assets/iconDown.svg"
 
 
 const StarProject = () => {
@@ -8,9 +9,9 @@ const StarProject = () => {
         <div className="star__project">
             <div className="star__projectSloy">
                 <div className="star__projectLeft">
-                    <h1 className="star__projectLeftTitle">Custom SaaS</h1>
-                    <h1 className="star__projectLeftTitle">Mobile and Web Development</h1>
-                    <h1 className="star__projectLeftTitle">UI/UX Design</h1>
+                    <h1 className="star__projectLeftTitle">Custom SaaS <img src={iconDown} /></h1>
+                    <h1 className="star__projectLeftTitle">Mobile and Web Development <img src={iconDown} /></h1>
+                    <h1 className="star__projectLeftTitle">UI/UX Design <img src={iconDown} /></h1>
                 </div>
                 <img src={background} alt="" className="star__imageBack"/>
             </div>
