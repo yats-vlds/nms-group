170227fabe50b798a274f0dc08e89da826f00e9e
Lexun <59420596+lexun1060@users.noreply.github.com>
2021-02-14 12:04:57 +0200

    complet component Ten

---
+++ src/components/FormRequest.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FormRequest = () => {
+  return (
+    <div className="form">
+        <input type="text" />
+        <input type="email" />
+        <textarea rows="10" cols="45" name="text"></textarea>
+    </div>
+  )
+}
+
+export default FormRequest
